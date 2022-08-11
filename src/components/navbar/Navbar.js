244ffaad680e29beba12f7a1767ab02cb83f4a7a
Sanket Mudholkar <93979254+sanket0708@users.noreply.github.com>
2022-08-11 23:05:28 +0530

    Navbar completed

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import { BiSearch } from "react-icons/bi";
+import { BsPerson } from "react-icons/bs";
+import { HiOutlineMenuAlt4 } from "react-icons/hi";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaPinterest,
+  FaTwitter,
+  FaYoutube,
+} from "react-icons/fa";
+import "./NavbarStyles.css";
+
+function Navbar() {
+  const [nav, setNav] = useState(false);
+  const handleNav = () => setNav(!nav);
+
+  return (
+    <div className={nav ? "navbar navbar-bg" : "navbar"}>
+      <div className="logo">
+        <h2>TRAVEL</h2>
+      </div>
+      <ul className="nav-menu">
+        <li>Home</li>
+        <li>Destinations</li>
+        <li>Travel</li>
+        <li>Book</li>
+        <li>Views</li>
+      </ul>
+
+      <div className="nav-icons">
+        <BiSearch className="icon" style={{ marginRight: "1rem" }} />
+        <BsPerson className="icon" />
+      </div>
+      <div className="hamburger" onClick={handleNav}>
+        <HiOutlineMenuAlt4 className="icon" />
+      </div>
+
+      <div className={nav ? "mobile-menu active" : "mobile-menu"}>
+        <ul className="mobile-nav">
+          <li>Home</li>
+          <li>Destinations</li>
+          <li>Travel</li>
+          <li>Book</li>
+          <li>Views</li>
+        </ul>
+        <div className="mobile-menu-bottom">
+          <div className="menu-icons">
+            <button>Search</button>
+            <button>Account</button>
+          </div>
+          <div className="social-icons">
+            <FaFacebook className="icon" />
+            <FaInstagram className="icon" />
+            <FaTwitter className="icon" />
+            <FaPinterest className="icon" />
+            <FaYoutube className="icon" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
